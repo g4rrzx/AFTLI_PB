aa4acb2b524c8cd6957e5f6e240f3cf20e515959
Tegar Andriansyah <100791610+g4rrzx@users.noreply.github.com>
2025-07-01 15:01:55 +0700

    Add files via upload

---
+++ AFTLI_PB/src/Anggota/DatabaseConnection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Anggota;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author TEGAR
+ */
+public class DatabaseConnection {
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3307/aftli_pb";
+            String username = "root";
+            String password = "";
+            
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Koneksi berhasil");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver MySQL tidak ditemukan: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
